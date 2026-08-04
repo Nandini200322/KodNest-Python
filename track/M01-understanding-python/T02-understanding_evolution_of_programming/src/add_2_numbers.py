a09da5d6308fd8d9ee386d8ca5ee4bc1be540dba
Nandini <nanduhs2003@gmail.com>
2026-08-04 21:46:02 +0530

    changes1

--- track/M01-understanding-python/T02-understanding_evolution_of_programming/src/add_2_numbers.py
+++
@@ -1,4 +0,0 @@
-first_number = 12
-second_number = 8
-total = first_number + second_number
-print(total)
---
+++ track/M02-Introduction to python/T01-Introduction to Python and Basic Constructs/src/add_two_number.py
@@ -0,0 +1,7 @@
+#Read the numbers
+a = int(input())
+b = int(input())
+# Calculate the sum
+sum = a + b
+# Display the result
+print(f"Sum: {sum}")
