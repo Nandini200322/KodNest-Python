a7aa255a274e5008466419f24323f0808f939b5b
Nandini <nanduhs2003@gmail.com>
2026-08-12 09:33:46 +0530

    changes2

---
+++ track/M02-Introduction to python/TO3-Lists, Tuples, Strings, Slicing and Mutability/src/create&unpack_studentRecord.py
@@ -0,0 +1,13 @@
+
+name = input()
+course = input()
+score = int(input())
+
+student_record = (name, course, score)
+
+student_name, student_course, student_status = student_record
+
+
+print(f"Name: {student_name}")
+print(f"Course: {student_course}")
+print(f"Score: {student_status}")
